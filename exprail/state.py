5abb07314cd7eec2c7eb50cf88c7620b375c65e7
Imre Piller <piller.imre@gmail.com>
2017-07-12 00:00:16 +0200

    Properties and exceptions for the State class

--- exprail/state.py
+++ exprail/state.py
@@ -8,14 +8,35 @@ class State:
 
     def __init__(self, grammar, expression_name='', node_id=None):
         self._grammar = grammar
-        self._expression_name = expression_name
-        self._node_id = node_id
-        if expression_name == '' and node_id is None:
-            self.calc_initial_state()
+        if expression_name == '':
+            if node_id is None:
+                self.calc_initial_state()
+            else:
+                raise ValueError('Unnecessary node identifier argument!')
+        elif expression_name in grammar.expressions:
+            self._expression_name = expression_name
+            if node_id in grammar[expression_name].nodes():
+                self._node_id = node_id
+            else:
+                raise ValueError('The "{}" expression does not contain the node {}'.format(expression_name, node_id))
+        else:
+            raise ValueError('The grammar has no "{}" expression!'.format(expression_name))
+
+    @property
+    def expression_name(self):
+        return self._expression_name
+
+    @property
+    def expression(self):
+        return self._grammar[self._expression_name]
+
+    @property
+    def node_id(self):
+        return self._node_id
 
     @property
     def node(self):
-        return self._grammar.expressions[self._expression_name].nodes[self._node_id]
+        return self.expression.nodes[self._node_id]
 
     def calc_initial_state(self):
         """Calculate the initial state of the given grammar."""
