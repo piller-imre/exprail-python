538f15bebec3aca8f370454742830bcbe14f3dbf
Imre Piller <piller.imre@gmail.com>
2017-07-11 23:45:41 +0200

    Test names and grammar for router testing

--- exprail/router.py
+++ exprail/router.py
@@ -16,10 +16,20 @@ def find_next_state(state, token):
     return available_states[0]
 
 
-def collect_available_states(self, state):
+def collect_available_states(state):
     """
     Collect the available states from the current state.
     :param state: the current state of the parser
     :return: the list of the available states
+    """
+    return []
+
+
+def find_router_nodes(node):
+    """
+    Find the next available router nodes from the current node.
+    The type of the router node is NodeType.ROUTER or NodeType.TOKEN.
+    :param node: the start node of the searching
+    :return: the list of router nodes
     """
     return []
--- tests/test_router.py
+++ tests/test_router.py
@@ -4,4 +4,26 @@ import unittest
 class RouterTest(unittest.TestCase):
     """Unittest for routing in the grammar"""
 
-    pass
+    def test_unique_available_state(self):
+        pass
+
+    def test_recurrent_next_state(self):
+        pass
+
+    def test_multiple_next_states(self):
+        pass
+
+    def test_find_finish_node(self):
+        pass
+
+    def test_find_unique_router_node(self):
+        pass
+
+    def test_find_current_router_node(self):
+        pass
+
+    def test_find_current_token_node(self):
+        pass
+
+    def test_multiple_router_nodes(self):
+        pass
